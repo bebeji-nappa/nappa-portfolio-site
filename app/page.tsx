ca84fc68ca05b049bb60a1f03a3cff6ec199ba7e
bebeji-nappa <masadecade@gmail.com>
2025-09-25 19:08:01 +0900

    fix: レスポンシブデザインに対応する

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,18 @@ export default function Home() {
   const bio = fs.readFileSync(bioPath, "utf8");
 
   return (
-    <div className="container w-4xl mx-auto mt-5">
-      <Image src="/logo.svg" alt="Logo" width={600} height={150} />
+    <div className="container max-w-4xl mx-auto mt-5 px-5 flex flex-col gap-8">
+      <div style={{ display: 'block', position: 'relative', width: '300px', height: '75px' }}>
+        <Image
+          src='/logo.svg'
+          alt='title_logo'
+          sizes='100vw'
+          fill
+          style={{
+            width: '100%'
+          }}
+        />
+      </div>
 
       <article>
         <Richmd text={bio} />
